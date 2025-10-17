4ddf8dce882d231e3bafda6868508123692eca54
shivangranawat <shivangranawat@itgeeks.com>
2025-10-17 13:35:43 +0530

    responsive-complete

--- js/script.js
+++ js/script.js
@@ -51,3 +51,28 @@ var swiper = new Swiper(".mySwiper3", {
         },
     },
 });
+
+
+var swiper = new Swiper(".mySwiper4", {
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    navigation: {
+        nextEl: ".swiper-button-next2",
+        prevEl: ".swiper-button-prev2",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 1,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 1,
+            spaceBetween: 50,
+        },
+    },
+});
